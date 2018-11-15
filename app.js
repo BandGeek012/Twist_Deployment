241afe23cef7bacd855b7cb802cf38d0ce70780c
Lab User <LabUser@labs.salina.k-state.edu>
2018-11-15 10:09:22 -0600

    11/15/2018 Connected to mLab

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+//Import the mongoose module
+var mongoose = require('mongoose');
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://Me:B4ndg33k@ds161183.mlab.com:61183/twist_practice';
+mongoose.connect(mongoDB);
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
