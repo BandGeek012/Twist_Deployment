3cb3ea87c21821755b3d40c637db77fa6d2c78e6
Bryan Weikle <weikle78@ksu.edu>
2018-11-24 20:05:59 -0600

    admin page working

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res) {
+  res.render('index', {title: 'Admin Page'});
+  //res.redirect('/admin');
+});
+
 //require controller modules.
 var schedule_controller = require('../controllers/scheduleController');
 var session_controller = require('../controllers/sessionController');
-
+var participant_controller = require('../controllers/participantController');
 //schedule routes
 
-//get admin homepage
-router.get('/', schedule_controller.index);
+module.exports = router;
